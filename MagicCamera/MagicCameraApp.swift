82547127264af76d2300d444eba981c3e014b8ae
Apoorv Verma <vermasunny224@gmail.com>
2023-10-09 22:45:37 +0530

    Initial Commit

---
+++ MagicCamera/MagicCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicCameraApp.swift
+//  MagicCamera
+//
+//  Created by Apoorv Verma on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MagicCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
